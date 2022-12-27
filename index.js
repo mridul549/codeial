da91a66ce4f589b0e25171c0c00c7501036e1cd5
mridul549 <mridulverma478@gmail.com>
2022-12-27 22:22:17 +0530

    Version 1

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path    = require('path');
 const port    = 3000;
 
 const app = express();
 
-app.get('/', function(req,res){
-    res.send('hello');
-})
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.use(express.urlencoded());
+app.use(express.static('public'));
+
+// Using express router
+// Now instead of writing the entire code in just one file, we are breaking the code
+// now all the different routes like '/', '/users', '/create-contact', will have a different js file
+// look in routes folder
+// this folder contains all the required routes, to access those routes, we use a middleware
+// this middleware points to the root route
+app.use('/', require('./routes/home'));
 
 app.listen(port, function(err){
     if(!err){
