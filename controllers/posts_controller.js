8b345592bcb68f4ed6e88ab8d2112d59cb7e2d14
mridul549 <mridulverma478@gmail.com>
2023-02-07 00:58:33 +0530

    Add async await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,28 +1,29 @@
 const Post = require('../models/post');
 const Comment = require('../models/comments');
 
-module.exports.createPosts = function(req,res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err){
-        if(err){
-            console.log('Error at creating posts');
-        }
-    })
+module.exports.createPosts = async function(req,res){
+    try {
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+    } catch (error) {
+        console.log(error);
+    }
     return res.redirect('/');
 }
 
-module.exports.destroyPost = function(req,res) {
-    Post.findById(req.params.id, function(err, post){
+module.exports.destroyPost = async function(req,res) {
+
+    try {
+        let post = await Post.findById(req.params.id);
         // .id means _id has been converted to string
         if(post.user == req.user.id){
             post.remove();
-            Comment.deleteMany({post: req.params.id}, function(err){
-                return res.redirect('back');
-            })
-        } else {
-            res.redirect('back');
+            Comment.deleteMany({post: req.params.id});
         }
-    })
+    } catch (error) {
+        console.log(error);
+    }
+    return res.redirect('back');
 }
